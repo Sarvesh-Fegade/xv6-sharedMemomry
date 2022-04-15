c182bf5cf68ad0a38451e2f9cba5b9e67c58d05f
Sarvesh.Fegade <fegadesb19.comp@coep.ac.in>
2022-04-16 01:51:40 +0530

    header files

--- sys/ipc.h
+++ sys/ipc.h
@@ -1,3 +1,8 @@
+#define IPC_CREAT  00001000   /* create if key is nonexistent */
+#define IPC_EXCL   00002000   /* fail if key exists */
+#define IPC_NOWAIT 00004000   /* return error on wait */
+
+
 struct ipc_perm {
     unsigned int __key;
     unsigned int mode;
--- sys/shm.h
+++ sys/shm.h
@@ -1,6 +1,15 @@
 #define MAX_PAGES   32
 #define MAX_REGION  128
 
+#define SHM_R		0400	/* or S_IRUGO from <linux/stat.h> */
+#define SHM_W		0200	/* or S_IWUGO from <linux/stat.h> */
+#define SHM_RW      0600
+
+#define	SHM_RDONLY	010000	/* read-only access */
+#define	SHM_RND		020000	/* round attach address to SHMLBA boundary */
+#define	SHM_REMAP	040000	/* take-over region on attach */
+#define	SHM_EXEC	0100000 /* execution access */
+
 
 
 struct shmid_ds {
